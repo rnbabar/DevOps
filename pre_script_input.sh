966dfae9dec9cca3ecda479bf3f3277b42980baa
Rab Nawaz <74647472+rnbabar@users.noreply.github.com>
2021-09-05 11:57:31 -0700

    Create pre_script_input.sh

---
+++ pre_script_input.sh
@@ -0,0 +1,7 @@
+#! bin/bash
+# The script to read values from command line
+echo 'First Argument'   $0
+echo 'Second Argument'  $1
+echo 'Third Argument'   $2
+echo 'Total Nos of  Argument'   $#
+echo 'All Argument'   $*
