8c7e6935fd23cdc6876a081e2bf0fc5b285feac7
Rab Nawaz <74647472+rnbabar@users.noreply.github.com>
2021-09-05 12:21:36 -0700

    Update io_redirect.sh

--- io_redirect.sh
+++ io_redirect.sh
@@ -1,4 +1,4 @@
 #! bin/bash
 # Scipt to store output and errors in file instead of screen 
-echo 'Name of the script' $1 STDOUT(>) input.dat
+echo 'Name of the script' $1 >> input.dat
 echo 'The error in the script' 2>input.dat
