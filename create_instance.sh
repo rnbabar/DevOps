a0f62a93d0a190939f59fc12a14d35fc5e0033ff
Rab Nawaz <74647472+rnbabar@users.noreply.github.com>
2021-09-07 00:19:46 -0700

    v3

--- create_instance.sh
+++ create_instance.sh
@@ -1,7 +1,19 @@
 #! bin/bash
-# To check the existing running instances
-Inst_Name=$1aws ec2 describe-instances --filters "Name=tag:Name,values=$Inst_Name" | jq .Reservation[].Instances[].state.Name | grep running &>/dev/null
-if [ $? -eq 0 ]; then
-echo "Instance $Inst_Name is already running "
-exit 0
+Inst_Name=$1
+TMPLID="lt-049de486aa5979b15"
+TMPLVER=3
+
+if [ -z "${Inst_Name}" ]; then
+
+    echo -e "You did not provided the Instance Name "
+    exit 1
+
 fi
+
+#aws ec2 run-instances --Launch-Template launchTemplateId=$TMPLID, version=$TMPLVER 
+3# To check the existing running instances
+#aws ec2 describe-instances --filters "Name=tag:Name,values=$Inst_Name" | jq .Reservation[].Instances[].state.Name | grep running &>/dev/null
+#if [ $? -eq 0 ]; then
+#echo "Instance $Inst_Name is already running "
+#exit 0
+#fi
