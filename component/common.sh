7d1106ad8faa12aaae0b9db5b89220f2a9bee9e7
Nawaz <rnbabar@me.com>
2021-09-12 12:41:28 -0700

    Final2

--- component/common.sh
+++ component/common.sh
@@ -7,9 +7,9 @@ print() {
 
 status_check() {
   if [ $1 -eq 0 ]; then 
-    echo -e "\e[32mSUCCESS\e[0m"
+    echo -e "\t\t\e[32mSUCCESS\e[0m"
   else 
-    echo -e "\e[31mFAILURE\e[0m"
+    echo -e "\t\t\e[31mFAILURE\e[0m"
     exit 2
   fi 
 }
