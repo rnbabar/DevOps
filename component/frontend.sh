507e84cb4738f662e8793eaa07e972da4564100f
Nawaz <rnbabar@me.com>
2021-09-14 00:59:01 -0700

    Modular script

--- component/frontend.sh
+++ component/frontend.sh
@@ -1,5 +1,17 @@
 #! bin/bash
-source common.sh
+print() {
+  echo -e "\n\e[36m----------------- $1 ----------------------\e[0m\n" >>$LOG
+  echo -n -e "$1 \t- "
+}
+
+status_check() {
+  if [ $1 -eq 0 ]; then 
+    echo -e "\t\e[32mSUCCESS\e[0m"
+  else 
+    echo -e "\t\e[31mFAILURE\e[0m"
+    exit 2
+  fi 
+}
 COMPONENT="frontend"
 #install nginx
 install_nginx
